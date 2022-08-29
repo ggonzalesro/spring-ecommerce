71bf5540552a49114362b7f84cfbafa0773902fe
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-29 15:00:26 -0500

    creación del metodo edit y update en ProductoController, creación de la vista EDIT

--- src/main/java/com/javacomunica/controller/ProductoController.java
+++ src/main/java/com/javacomunica/controller/ProductoController.java
@@ -1,10 +1,13 @@
 package com.javacomunica.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +42,25 @@ public class ProductoController {
 		Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(usuario);
 		productoService.save(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProduct=productoService.get(id);
+		producto = optionalProduct.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		
+		model.addAttribute("producto",producto);
+		
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);
 		return "redirect:/productos";
 	}
 }
