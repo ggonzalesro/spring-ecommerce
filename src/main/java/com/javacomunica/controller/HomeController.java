13f0dbdf50b1480c3cf042bba6bc675c3ed5b040
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-30 17:41:56 -0500

    completa la funcionalidad del metodo productoHome del controlador HomeController, adicional hemos realizado que se muestre la informacion en la vista productohome

--- src/main/java/com/javacomunica/controller/HomeController.java
+++ src/main/java/com/javacomunica/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.javacomunica.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javacomunica.model.Producto;
 import com.javacomunica.service.ProductoService;
 
 @Controller
@@ -29,9 +32,17 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		
 		log.info("el id de producto enviado como parametro {}",id);
+		
+		Producto producto = new Producto();
+		
+		Optional<Producto> produOptional=productoService.get(id);
+		
+		producto = produOptional.get();
+		
+		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
 	}
