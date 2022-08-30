14c4985979ce886b5c0da38f3d220b701c46b47c
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-30 17:23:33 -0500

    creacion de vista productohome, creacion de la funcionalidad incompleta del metodo productoHome del controlador HomeController

--- src/main/java/com/javacomunica/controller/HomeController.java
+++ src/main/java/com/javacomunica/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.javacomunica.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javacomunica.service.ProductoService;
@@ -12,6 +15,8 @@ import com.javacomunica.service.ProductoService;
 @RequestMapping("/")
 public class HomeController {
 
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;
 	
@@ -23,5 +28,12 @@ public class HomeController {
 		return "usuario/home";
 	}
 	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		
+		log.info("el id de producto enviado como parametro {}",id);
+		
+		return "usuario/productohome";
+	}
 	
 }
