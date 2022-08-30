ab869b027f5d42993fa79253c37a002cdef8aaab
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-30 18:53:29 -0500

    creacion de la funcionalidad para eliminar un producto del carrito de compras

--- src/main/java/com/javacomunica/controller/HomeController.java
+++ src/main/java/com/javacomunica/controller/HomeController.java
@@ -27,70 +27,97 @@ import com.javacomunica.service.ProductoService;
 public class HomeController {
 
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
-	//esto sirve para almacenar 1 o más detalles de la ORDEN
-	List<DetalleOrden> detalles=new ArrayList<DetalleOrden>();
 
-	//almacena los datos de la ORDEN
+	// esto sirve para almacenar 1 o más detalles de la ORDEN
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+	// almacena los datos de la ORDEN
 	Orden orden = new Orden();
-	
+
 	@GetMapping("")
 	public String home(Model model) {
-		
+
 		model.addAttribute("productos", productoService.findAll());
-		
+
 		return "usuario/home";
 	}
-	
+
 	@GetMapping("productohome/{id}")
 	public String productoHome(@PathVariable Integer id, Model model) {
-		
-		log.info("el id de producto enviado como parametro {}",id);
-		
+
+		log.info("el id de producto enviado como parametro {}", id);
+
 		Producto producto = new Producto();
-		
-		Optional<Producto> produOptional=productoService.get(id);
-		
+
+		Optional<Producto> produOptional = productoService.get(id);
+
 		producto = produOptional.get();
-		
+
 		model.addAttribute("producto", producto);
-		
+
 		return "usuario/productohome";
 	}
-	
+
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id,@RequestParam Integer cantidad,Model model) {
-		
-		DetalleOrden detalleOrden=new DetalleOrden();
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+
+		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
-		double sumaTotal=0;
-		
-		Optional<Producto> produOptional=productoService.get(id);
-		
-		log.info("producto añadido: {}",produOptional.get());
-		log.info("cantidad añadida: {}",cantidad);
-		
+		double sumaTotal = 0;
+
+		Optional<Producto> produOptional = productoService.get(id);
+
+		log.info("producto añadido: {}", produOptional.get());
+		log.info("cantidad añadida: {}", cantidad);
+
 		producto = produOptional.get();
-		
+
 		detalleOrden.setCantidad(cantidad);
 		detalleOrden.setPrecio(producto.getPrecio());
 		detalleOrden.setNombre(producto.getNombre());
-		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
-		
+
 		detalles.add(detalleOrden);
-		
-		sumaTotal=detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
-		
+
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
 		orden.setTotal(sumaTotal);
-		
-		model.addAttribute("cart",detalles);
+
+		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
-		
+
 		return "usuario/carrito";
 	}
-	
+
+	// remover un producto del carrito
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductCart(@PathVariable Integer id, Model model) {
+
+		// lista nueva de productos
+		List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNuevas.add(detalleOrden);
+			}
+		}
+
+		// poner la nueva lista con los productos que no fueron eliminados
+		detalles = ordenesNuevas;
+
+		double sumaTotal = 0;
+
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+		orden.setTotal(sumaTotal);
+
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+
+		return "usuario/carrito";
+	}
 }
