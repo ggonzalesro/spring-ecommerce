21b5e38987ddfac6088796546ae9362bd646f4c2
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-31 15:40:55 -0500

    creacion de la funcionalidad para autogenerar el numero de orden

--- src/main/java/com/javacomunica/service/IOrdenService.java
+++ src/main/java/com/javacomunica/service/IOrdenService.java
@@ -1,7 +1,11 @@
 package com.javacomunica.service;
 
+import java.util.List;
+
 import com.javacomunica.model.Orden;
 
 public interface IOrdenService {
 	Orden save(Orden orden);
+	
+	List<Orden> findAll();
 }
