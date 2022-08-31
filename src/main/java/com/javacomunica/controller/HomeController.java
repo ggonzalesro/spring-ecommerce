431d4af58cd3a443fde86c5e51fba86cb7efab45
GabrielGR <gabrielgonzalesroca@gmail.com>
2022-08-30 21:24:50 -0500

    creación de la funcionalidad para poder enlazarnos de manera directa al carrito de compras desde cualquier parte de la navegacion de usuario, creación de la funcionalidad para evitar que se repita el producto en el detalle

--- src/main/java/com/javacomunica/controller/HomeController.java
+++ src/main/java/com/javacomunica/controller/HomeController.java
@@ -81,7 +81,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 
-		detalles.add(detalleOrden);
+		// validar que producto no se agregue repetido
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -120,4 +126,12 @@ public class HomeController {
 
 		return "usuario/carrito";
 	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "/usuario/carrito";
+	}
+	
 }
